d872c4c6402d841a117db833cb0ab8effbb28870
Ghost <koffijude33@gmail.com>
2025-11-24 00:10:04 +0000

    jwt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,20 +6,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Register custom middleware aliases
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
             'photographer' => \App\Http\Middleware\CheckPhotographer::class,
             'admin' => \App\Http\Middleware\CheckAdmin::class,
-        ]);
-        $middleware->api(prepend: [
-            \Illuminate\Http\Middleware\HandleCors::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
